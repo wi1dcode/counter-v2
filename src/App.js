ae6715b7fc18099dedbc488273f0d5c6f3ed9302
wi1dcode <magosdk@icloud.com>
2022-08-01 14:03:47 +0200

    counter v2 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
 
 function App() {
+  const [count, setCount] = useState(1)
+
+  
+  const handleButtonPlusClick = () => {
+    setCount(count + 1)
+  }
+
+  
+  const handleButtonMinusClick = () => {
+    setCount(count - 1)
+  }
+
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{count}</div>
+      <button onClick={handleButtonMinusClick}>-</button>
+      <button onClick={handleButtonPlusClick}>+</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
